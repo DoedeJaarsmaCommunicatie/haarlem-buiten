5d83c582e4f802361d821f79b387f463e780ff7c
Pridestalker <6103741+Pridestalker@users.noreply.github.com>
2020-04-15 12:35:23 +0200

    Added suggestions of hybridsjs author dominic

--- assets/elements/separator/separator.component.js
+++ assets/elements/separator/separator.component.js
@@ -1,38 +1,35 @@
-import {
-	html,
-	property
-} from 'hybrids';
+import { html } from 'hybrids';
 
 export default  {
-	spaceTop: property(false),
-	spaceBottom: property(false),
+	spaceTop: false,
+	spaceBottom: false,
 	render: ({ spaceTop, spaceBottom }) => html`
-<hr class="${spaceBottom? 'space-bottom' : ''} ${spaceTop ? 'space-top' : ''}">
+<hr class="${{'space-bottom': spaceBottom, 'space-top': spaceTop}}" >
 <style>
-hr {
-	border: none;
-	border-top: 7px solid #f4f4f4;
-	margin: 0;
-	display: block;
-	box-sizing: content-box;
-	height: 0;
-	overflow: visible;
-}
-
-hr.space-top {
-	margin-top: 1.5rem;
-}
-
-hr.space-bottom {
-	margin-bottom: 1.5rem;
-}
-
-@media screen and (max-width: 768px) {
-hr {
-	margin-left: 1rem;
-	margin-right: 1rem;
-}
-}
+	hr {
+		border: none;
+		border-top: 7px solid #f4f4f4;
+		margin: 0;
+		display: block;
+		box-sizing: content-box;
+		height: 0;
+		overflow: visible;
+	}
+	
+	hr.space-top {
+		margin-top: 1.5rem;
+	}
+	
+	hr.space-bottom {
+		margin-bottom: 1.5rem;
+	}
+	
+	@media screen and (max-width: 768px) {
+		hr {
+			margin-left: 1rem;
+			margin-right: 1rem;
+		}
+	}
 </style>
 `
 }
